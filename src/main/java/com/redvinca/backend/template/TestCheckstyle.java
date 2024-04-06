32bded8b14756497812288e1cf56c7cffe7862b2
Karan Balkar <karanbalkar@gmail.com>
2024-04-06 18:31:57 +0530

    initial commit

---
+++ src/main/java/com/redvinca/backend/template/TestCheckstyle.java
@@ -0,0 +1,14 @@
+public class TestCheckstyle {
+    
+    private static void printOrder() {
+        System.out.println("print order usage correct");
+    }
+
+    /*private void PrintOrder() {
+        System.out.println("print order usage incorrect");
+    }*/
+
+    /*public static void main(String[] args) {
+        printOrder();
+    }*/
+}
---
+++ src/test/java/com/redvinca/backend/template/BackendTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redvinca.backend.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
