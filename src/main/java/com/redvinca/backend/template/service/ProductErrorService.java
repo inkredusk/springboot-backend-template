4b0ac4d413d87512a95a1b99ecc78747cce9847b
Karan Balkar <karanbalkar@gmail.com>
2024-04-25 09:04:36 +0530

    added custom message for checkstyle rules

---
+++ src/main/java/com/redvinca/backend/template/service/ProductErrorService.java
@@ -0,0 +1,18 @@
+package com.redvinca.backend.template.service;
+
+public class ProductErrorService {
+    
+    //public static final String errormessage = "Product Error";
+
+     public String GetErrorMessage() {
+         return "Product Error";
+     }
+
+    /*public String handleError() {
+       return errormessage;
+    }*/
+
+    // public String getErrorMessage() {
+    //     return "Product Error";
+    // }
+}
